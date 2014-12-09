8098cf7e2a3b11b5a332bd76801778a3aca2f7b5
Patrick Hoolboom <patrick@stackstorm.com>
2014-12-09 18:45:45 +0000

    Updated AWS pack

--- packs/aws/actions/lib/action.py
+++ packs/aws/actions/lib/action.py
@@ -1,13 +1,18 @@
-from lib import ec2
+import imp, re, importlib
 from st2actions.runners.pythonrunner import Action
-
+import os, yaml
 
 class BaseAction(Action):
 
     def __init__(self, config):
         super(BaseAction, self).__init__(config)
-        self.ec2 = ec2.EC2(config)
-    
-    def ec2_action(self,action,**kwargs):
-      act = self.ec2.get_object(action)
-      return act(self.ec2.conn,**kwargs)
+        self.setup = config['setup']
+
+    def do_method(self,module_path, cls, action,**kwargs):
+      module = importlib.import_module(module_path)
+      obj = getattr(module,cls)(**self.setup)
+      return getattr(obj,action)(**kwargs)
+
+    def do_function(self,module_path,action,**kwargs):
+      module = __import__(module_path)
+      return getattr(module,action)(**kwargs)
