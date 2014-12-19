c8a4cdcb3fb57b7138150b3d2e77f26d912faab0
James Fryman <james@fryman.io>
2014-12-19 14:18:50 -0600

    Add git branch to build job action

--- packs/jenkins/actions/build_job.py
+++ packs/jenkins/actions/build_job.py
@@ -2,5 +2,4 @@ from lib import action
 
 class BuildProject(action.JenkinsBaseAction):
     def run(self, project, branch="master"):
-        return self.jenkins.build_job(project)
-
+        return self.jenkins.build_job(project, {'branch': branch})
