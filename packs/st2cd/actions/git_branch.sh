5ad26d349bf17e17f8a8fa48fab4ed698cceb12a
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2015-01-08 17:31:55 -0800

    Add st2 action to git branch, change version and commit

---
+++ packs/st2cd/actions/git_branch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+GIT=`which git`
+REPO=$1
+DATE=`date +%s`
+VERSION=$2
+BRANCH=v$2
+FILE="st2common/st2common/__init__.py"
+OUTPUT=/tmp/git-output-$DATE
+
+cd ${REPO}
+OUT=`$GIT pull origin master -q > $OUTPUT && $GIT branch ${BRANCH} -q && $GIT checkout ${BRANCH} -q`
+if [[ $? == 0 ]]
+then
+  VERSIONOUT=`sed -i '' "s/\(__version__ = \).*/\1'${VERSION}'/" ${FILE}`
+  if [[ $? == 0 ]]
+  then
+    OUT=`$GIT add $FILE && $GIT commit -aqm "Cutting branch for release - ${VERSION}" && $GIT push origin -q $BRANCH`
+    if [[ $? == 0 ]]
+    then
+      echo ${BRANCH}
+    else
+      cat ${OUTPUT}
+      rm ${OUTPUT}
+    fi
+  else
+    cat ${OUTPUT}
+    rm ${OUTPUT}
+  fi
+else
+  cat ${OUTPUT}
+  rm ${OUTPUT}
+fi
