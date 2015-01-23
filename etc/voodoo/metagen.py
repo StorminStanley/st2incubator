9c3c65c492aae9732a2609fe01866444c115876c
manasdk <manas@stackstorm.com>
2015-01-23 02:16:58 +0000

    Pep8 compliant formatting.

--- etc/voodoo/metagen.py
+++ etc/voodoo/metagen.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
-import pprint, os, shutil, sys
-import inspect, json, yaml, re, argparse, imp
+import pprint
+import os
+import shutil
+import sys
+import inspect
+import json
+import yaml
+import re
+import argparse
+import imp
 import importlib
 
-parser = argparse.ArgumentParser(description='StackStorm Action Metadata Generator for Python modules')
+parser = argparse.ArgumentParser(
+    description='StackStorm Action Metadata Generator for Python modules')
 
 parser.add_argument('--module', action="store", dest="module", default=None)
 parser.add_argument('--class', action="store", dest="clss")
@@ -30,20 +39,21 @@ if args.module is None and args.empty is False:
 elif args.module is not None and args.empty is False:
     module = importlib.import_module(args.module)
     if args.clss is not None:
-        obj = getattr(module,args.clss)
+        obj = getattr(module, args.clss)
     else:
         obj = module
 
 if args.ignore:
-  ignores = args.ignore.split(',')
+    ignores = args.ignore.split(',')
 else:
-  ignores = []
+    ignores = []
+
 
 def parseAdditional(adds):
     add_dict = {}
     for add in adds.split():
-        if re.search('=',add):
-            k,v = adds.split('=',1)
+        if re.search('=', add):
+            k, v = adds.split('=', 1)
             add_dict[k] = v
         else:
             add_dict[add] = None
@@ -55,7 +65,8 @@ if args.required is not None:
 if args.optional is not None:
     add_optional = parseAdditional(args.optional)
 
-def create_pack(pack,empty=True):
+
+def create_pack(pack, empty=True):
     pack_dir = 'output/packs/%s' % pack
     if os.path.isdir(pack_dir):
         shutil.rmtree(pack_dir)
@@ -63,7 +74,7 @@ def create_pack(pack,empty=True):
     os.mkdir("%s/rules" % pack_dir)
     os.mkdir("%s/sensors" % pack_dir)
     if empty is False:
-        shutil.copytree('actions',pack_dir + "/actions")
+        shutil.copytree('actions', pack_dir + "/actions")
     else:
         os.mkdir("%s/actions" % pack_dir)
 
@@ -76,8 +87,9 @@ def create_manifest(pack):
     manifest['author'] = args.author
     manifest['email'] = args.email
     fh = open('output/packs/%s/pack.yaml' % pack, 'w')
-    fh.write(yaml.dump(manifest,default_flow_style=False))
+    fh.write(yaml.dump(manifest, default_flow_style=False))
     fh.close()
+
 
 def create_md(pack):
     readme = open('output/packs/%s/README.md' % pack, 'w')
@@ -92,101 +104,106 @@ def create_md(pack):
     config.write("---\n")
     config.close()
 
+
 def get_all(modpath):
-  items = {}
-  pattern = re.compile('[^_].*')
-  for member in dir(modpath):
-    if pattern.match(member) and member not in ignores:
-      foo = getattr(modpath,member)
-      if inspect.ismethod(foo) is True or inspect.isfunction(foo) is True:
-        items[member] = {}
-        argspec = inspect.getargspec(foo)
-        if argspec.defaults is not None:
-          items[member] = dict(zip(argspec.args[-len(argspec.defaults):],argspec.defaults))
-        for arg in argspec.args:
-          if arg not in items[member] and arg not in ['self','names']:
-            items[member][arg] = 'required'
-        for item in items:
-          for p in items[item]:
-            if isinstance(items[item][p], tuple):
-                items[item][p] = map(list, items[item][p])
-        for req in add_required:
-            items[member][req] = 'required'
-        for opt in add_optional:
-            if add_optional[opt] is not None:
-                items[member][opt] = add_optional[opt]
-            else:
-                items[member][opt] = None
-         
-  return items
+    items = {}
+    pattern = re.compile('[^_].*')
+    for member in dir(modpath):
+        if pattern.match(member) and member not in ignores:
+            foo = getattr(modpath, member)
+            if inspect.ismethod(foo) is True or inspect.isfunction(foo) is True:
+                items[member] = {}
+                argspec = inspect.getargspec(foo)
+                if argspec.defaults is not None:
+                    items[member] = dict(
+                        zip(argspec.args[-len(argspec.defaults):], argspec.defaults))
+                for arg in argspec.args:
+                    if arg not in items[member] and arg not in ['self', 'names']:
+                        items[member][arg] = 'required'
+                for item in items:
+                    for p in items[item]:
+                        if isinstance(items[item][p], tuple):
+                            items[item][p] = map(list, items[item][p])
+                for req in add_required:
+                    items[member][req] = 'required'
+                for opt in add_optional:
+                    if add_optional[opt] is not None:
+                        items[member][opt] = add_optional[opt]
+                    else:
+                        items[member][opt] = None
+
+    return items
+
 
 def build_action_list(obj):
-  items = get_all(obj)
-  actions = {'items': items}
-  actions['module_path'] = obj.__module__
-  if inspect.isclass(obj) is True:
-    actions['cls'] = obj.__name__
-  return actions
+    items = get_all(obj)
+    actions = {'items': items}
+    actions['module_path'] = obj.__module__
+    if inspect.isclass(obj) is True:
+        actions['cls'] = obj.__name__
+    return actions
 
-def generate_meta(obj,pack):
 
-  actions = build_action_list(obj)
+def generate_meta(obj, pack):
 
-  class_param = {}
-  if 'cls' in actions.keys():
-    class_param = {"cls": {"type": "string", "immutable": True, "default": actions['cls']}}
-  module_param = {"module_path": {"type": "string", "immutable": True, "default": actions['module_path']}}
-  for action in actions['items']:
-    if args.action_prefix is not None:
-        prefix = "%s_" % args.action_prefix
-    else:
-        prefix = ""
+    actions = build_action_list(obj)
 
-    parameters = {}
-    action_meta = {
-      "name": "",
-      "parameters":{
-        "action": {
-          "type": "string",
-          "immutable": True,
-          "default": action }
-      },
-      "runner_type":"run-python",
-      "description":"",
-      "enabled":True,
-      "entry_point":"run.py"}
-
-    action_meta["name"] = "%s%s" % (prefix, action)
-    for parameter in actions['items'][action]:
-      parameters[parameter] = { "type":"string" }
-      if isinstance(actions['items'][action][parameter], bool):
-        parameters[parameter]['type'] = "boolean"
-      if actions['items'][action][parameter] is not None:
-        if actions['items'][action][parameter] == 'required':
-            parameters[parameter]['required'] = True
-        elif isinstance(actions['items'][action][parameter], list):
-            parameters[parameter]['type'] = "array"
-            parameters[parameter]['default'] = actions['items'][action][parameter]
+    class_param = {}
+    if 'cls' in actions.keys():
+        class_param = {"cls": {"type": "string", "immutable": True, "default": actions['cls']}}
+    module_param = {
+        "module_path": {"type": "string", "immutable": True, "default": actions['module_path']}}
+    for action in actions['items']:
+        if args.action_prefix is not None:
+            prefix = "%s_" % args.action_prefix
         else:
-            parameters[parameter]['default'] = actions['items'][action][parameter]
-    if class_param is not None:
-      parameters.update(class_param)
-    action_meta['parameters'].update(module_param)
-    action_meta['parameters'].update(parameters)
+            prefix = ""
 
-    filename = "output/packs/" + args.pack + "/actions/" + prefix + action + ".yaml"
+        parameters = {}
+        action_meta = {
+            "name": "",
+            "parameters": {
+                "action": {
+                    "type": "string",
+                    "immutable": True,
+                    "default": action}
+            },
+            "runner_type": "run-python",
+            "description": "",
+            "enabled": True,
+            "entry_point": "run.py"}
 
-    if not args.dry_run:
-      fh = open(filename, 'w')
-      fh.write(yaml.dump(action_meta,default_flow_style=False))
-      fh.close()
-    else:
-      print filename
-      print(yaml.dump(action_meta,default_flow_style=False))
+        action_meta["name"] = "%s%s" % (prefix, action)
+        for parameter in actions['items'][action]:
+            parameters[parameter] = {"type": "string"}
+            if isinstance(actions['items'][action][parameter], bool):
+                parameters[parameter]['type'] = "boolean"
+            if actions['items'][action][parameter] is not None:
+                if actions['items'][action][parameter] == 'required':
+                    parameters[parameter]['required'] = True
+                elif isinstance(actions['items'][action][parameter], list):
+                    parameters[parameter]['type'] = "array"
+                    parameters[parameter]['default'] = actions['items'][action][parameter]
+                else:
+                    parameters[parameter]['default'] = actions['items'][action][parameter]
+        if class_param is not None:
+            parameters.update(class_param)
+        action_meta['parameters'].update(module_param)
+        action_meta['parameters'].update(parameters)
 
-create_pack(args.pack,empty=args.empty)
+        filename = "output/packs/" + args.pack + "/actions/" + prefix + action + ".yaml"
+
+        if not args.dry_run:
+            fh = open(filename, 'w')
+            fh.write(yaml.dump(action_meta, default_flow_style=False))
+            fh.close()
+        else:
+            print filename
+            print(yaml.dump(action_meta, default_flow_style=False))
+
+create_pack(args.pack, empty=args.empty)
 create_manifest(args.pack)
 create_md(args.pack)
 
 if args.empty is not True:
-    generate_meta(obj,args.pack)
+    generate_meta(obj, args.pack)
