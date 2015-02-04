ea76b8197f1c35a0f5fdc75034a118ca5cd8bcfa
Andrey Belokrylov <mr_whitewings@yahoo.com>
2015-02-04 14:36:46 -0800

    renamed action name and its params to plural form

--- packs/vsphere/actions/get_vms.py
+++ packs/vsphere/actions/get_vms.py
@@ -3,9 +3,9 @@ from pyVmomi import vim
 from vmwarelib import inventory
 from vmwarelib.actions import BaseAction
 
-class GetVM(BaseAction):
+class GetVMs(BaseAction):
 
-    def run(self, id=None, name=None, datastore=None, datastore_cluster=None, resource_pool=None, vapp=None, host=None, folder=None, cluster=None, datacenter=None, virtual_switch=None, no_recursion=False ):
+    def run(self, ids=None, names=None, datastores=None, datastore_clusters=None, resource_pools=None, vapps=None, hosts=None, folders=None, clusters=None, datacenters=None, virtual_switches=None, no_recursion=False ):
         #TODO: food for thought. PowerCli contains additional parameters that are not present here for the folliwing reason:
         #<server> - we may need to bring it in if we decide to have connections to more than 1 VC.
         #<tag>    - Tags in VC are not the same as tags you see in Web Client for the reason, that those tags are stored in Inventory Service only. PowerCli somehow can access it, from vSphere SDK there is no way.
@@ -17,36 +17,36 @@ class GetVM(BaseAction):
         moid_to_vm = {}
 
         vms_from_vmids = []
-        if id:
-            vm_moids = [moid.strip() for moid in id.split(',')]
+        if ids:
+            vm_moids = [moid.strip() for moid in ids.split(',')]
             vms_from_vmids = [vim.VirtualMachine(moid, stub=si._stub) for moid in vm_moids]
             GetVM.__add_vm_properties_to_map_from_vm_array(moid_to_vm, vms_from_vmids)
 
         #getting vms from datastore objects
         vms_from_datastores = []
-        if datastore:
-            datastore_moids = [moid.strip() for moid in datastore.split(',')]
-            datastores = [vim.Datastore(moid, stub=si._stub) for moid in datastore_moids]
-            for ds in datastores:
+        if datastores:
+            datastore_moids = [moid.strip() for moid in datastores.split(',')]
+            vim_datastores = [vim.Datastore(moid, stub=si._stub) for moid in datastore_moids]
+            for ds in vim_datastores:
                 vms_from_datastores.extend(ds.vm)
             GetVM.__add_vm_properties_to_map_from_vm_array(moid_to_vm, vms_from_datastores)
 
         #getting vms from datastore cluster objects
         vms_from_datastore_clusters = []
-        if datastore_cluster:
-            datastore_cluster_moids = [moid.strip() for moid in datastore_cluster.split(',')]
-            datastore_clusters = [vim.StoragePod(moid, stub=si._stub) for moid in datastore_cluster_moids]
-            for ds_cl in datastore_clusters:
+        if datastore_clusters:
+            datastore_cluster_moids = [moid.strip() for moid in datastore_clusters.split(',')]
+            vim_datastore_clusters = [vim.StoragePod(moid, stub=si._stub) for moid in datastore_cluster_moids]
+            for ds_cl in vim_datastore_clusters:
                 for ds in ds_cl.childEntity:
                   vms_from_datastore_clusters.extend(ds.vm)
             GetVM.__add_vm_properties_to_map_from_vm_array(moid_to_vm, vms_from_datastore_clusters)
 
         #getting vms from virtual switch objects
         vms_from_virtual_switches = []
-        if virtual_switch:
-            virtual_switch_moids = [moid.strip() for moid in virtual_switch.split(',')]
-            virtual_switches = [vim.DistributedVirtualSwitch(moid, stub=si._stub) for moid in virtual_switch_moids]
-            for vswitch in virtual_switches:
+        if virtual_switches:
+            virtual_switch_moids = [moid.strip() for moid in virtual_switches.split(',')]
+            vim_virtual_switches = [vim.DistributedVirtualSwitch(moid, stub=si._stub) for moid in virtual_switch_moids]
+            for vswitch in vim_virtual_switches:
                 for pg in vswitch.portgroup:
                   vms_from_virtual_switches.extend(pg.vm)
             GetVM.__add_vm_properties_to_map_from_vm_array(moid_to_vm, vms_from_virtual_switches)
@@ -55,28 +55,28 @@ class GetVM(BaseAction):
         vms_from_containers = []
         containers = []
 
-        if resource_pool:
-            container_moids = [moid.strip() for moid in resource_pool.split(',')]
+        if resource_pools:
+            container_moids = [moid.strip() for moid in resource_pools.split(',')]
             containers += [vim.ResourcePool(moid, stub=si._stub) for moid in container_moids]
 
-        if vapp:
-            container_moids = [moid.strip() for moid in vapp.split(',')]
+        if vapps:
+            container_moids = [moid.strip() for moid in vapps.split(',')]
             containers += [vim.VirtualApp(moid, stub=si._stub) for moid in container_moids]
 
-        if host:
-            container_moids = [moid.strip() for moid in host.split(',')]
+        if hosts:
+            container_moids = [moid.strip() for moid in hosts.split(',')]
             containers += [vim.HostSystem(moid, stub=si._stub) for moid in container_moids]
 
-        if folder:
-            container_moids = [moid.strip() for moid in folder.split(',')]
+        if folders:
+            container_moids = [moid.strip() for moid in folders.split(',')]
             containers += [vim.Folder(moid, stub=si._stub) for moid in container_moids]
 
-        if cluster:
-            container_moids = [moid.strip() for moid in cluster.split(',')]
+        if clusters:
+            container_moids = [moid.strip() for moid in clusters.split(',')]
             containers += [vim.ComputeResource(moid, stub=si._stub) for moid in container_moids]
 
-        if datacenter:
-            container_moids = [moid.strip() for moid in datacenter.split(',')]
+        if datacenters:
+            container_moids = [moid.strip() for moid in datacenters.split(',')]
             containers += [vim.Datacenter(moid, stub=si._stub) for moid in container_moids]
 
         for cont in containers:
