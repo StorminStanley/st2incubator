e10449382c89017a5726d0307fd7feeeafb44e50
Andrey Belokrylov <mr_whitewings@yahoo.com>
2015-02-04 10:57:50 -0800

    adding retrieval of vms by id

--- packs/vsphere/actions/get_vm.py
+++ packs/vsphere/actions/get_vm.py
@@ -5,13 +5,22 @@ from vmwarelib.actions import BaseAction
 
 class GetVM(BaseAction):
 
-    def run(self, name=None, datastore=None, datastore_cluster=None, resource_pool=None, vapp=None, host=None, folder=None, cluster=None, datacenter=None, virtual_switch=None, no_recursion=False ):
+    def run(self, id=None, name=None, datastore=None, datastore_cluster=None, resource_pool=None, vapp=None, host=None, folder=None, cluster=None, datacenter=None, virtual_switch=None, no_recursion=False ):
+        #TODO: food for thought. PowerCli contains additional parameters that are not present here for the folliwing reason:
+        #<server> - we may need to bring it in if we decide to have connections to more than 1 VC.
+        #<tag>    - Tags in VC are not the same as tags you see in Web Client for the reason, that those tags are stored in Inventory Service only. PowerCli somehow can access it, from vSphere SDK there is no way.
 
         si = self.si
         si_content = si.RetrieveContent()
         props = ['name', 'runtime.powerState']
         result = []
         moid_to_vm = {}
+
+        vms_from_vmids = []
+        if id:
+            vm_moids = [moid.strip() for moid in id.split(',')]
+            vms_from_vmids = [vim.VirtualMachine(moid, stub=si._stub) for moid in vm_moids]
+            GetVM.__add_vm_properties_to_map_from_vm_array(moid_to_vm, vms_from_vmids)
 
         #getting vms from datastore objects
         vms_from_datastores = []
@@ -20,8 +29,7 @@ class GetVM(BaseAction):
             datastores = [vim.Datastore(moid, stub=si._stub) for moid in datastore_moids]
             for ds in datastores:
                 vms_from_datastores.extend(ds.vm)
-
-        GetVM.__add_vm_properties_to_map_from_vm_array(moid_to_vm, vms_from_datastores)
+            GetVM.__add_vm_properties_to_map_from_vm_array(moid_to_vm, vms_from_datastores)
 
         #getting vms from datastore cluster objects
         vms_from_datastore_clusters = []
@@ -31,8 +39,7 @@ class GetVM(BaseAction):
             for ds_cl in datastore_clusters:
                 for ds in ds_cl.childEntity:
                   vms_from_datastore_clusters.extend(ds.vm)
-
-        GetVM.__add_vm_properties_to_map_from_vm_array(moid_to_vm, vms_from_datastore_clusters)
+            GetVM.__add_vm_properties_to_map_from_vm_array(moid_to_vm, vms_from_datastore_clusters)
 
         #getting vms from virtual switch objects
         vms_from_virtual_switches = []
@@ -42,8 +49,7 @@ class GetVM(BaseAction):
             for vswitch in virtual_switches:
                 for pg in vswitch.portgroup:
                   vms_from_virtual_switches.extend(pg.vm)
-
-        GetVM.__add_vm_properties_to_map_from_vm_array(moid_to_vm, vms_from_virtual_switches)
+            GetVM.__add_vm_properties_to_map_from_vm_array(moid_to_vm, vms_from_virtual_switches)
 
         #getting vms from containers (location param)
         vms_from_containers = []
@@ -73,7 +79,6 @@ class GetVM(BaseAction):
             container_moids = [moid.strip() for moid in datacenter.split(',')]
             containers += [vim.Datacenter(moid, stub=si._stub) for moid in container_moids]
 
-        print containers
         for cont in containers:
             objView = si_content.viewManager.CreateContainerView(cont, [vim.VirtualMachine], not no_recursion)
             tSpec = vim.PropertyCollector.TraversalSpec(name='tSpecName', path='view', skip=False, type=vim.view.ContainerView)
