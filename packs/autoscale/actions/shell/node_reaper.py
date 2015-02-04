31a706ae561757fb63d7905a4df56cd58e69bb0d
James <james@fryman.io>
2015-02-04 14:27:14 +0000

    cleanup

--- packs/autoscale/actions/shell/node_reaper.py
+++ packs/autoscale/actions/shell/node_reaper.py
@@ -1,13 +1,15 @@
 from st2actions.runners.pythonrunner import Action
+import json
 
 class NodeReaperAction(Action):
     def run(self, nodes, count):
-        hostlist = []
-        for node in nodes:
-            if 'id' in host:
-                hostlist.append('id')
-
-        if count == 0:
-            return hostlist
-        else:
-            return hostlist[:count]
+        pass
+#        hostlist = []
+#        for node in nodes:
+#            if 'id' in host:
+#                hostlist.append('id')
+#
+#        if count == 0:
+#            return hostlist
+#        else:
+#            return hostlist[:count]
