d67c1bd6df663ee03833b40d377565918159aaa7
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2015-03-10 16:30:46 -0700

    Shutdown st2, clean mongo in teardown action

--- packs/st2cd/actions/teardown_mistral_itests.sh
+++ packs/st2cd/actions/teardown_mistral_itests.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+REPO=$1
+DROP_DBS=$2
+
+if [[ ! -d $REPO ]]
+then
+    echo "ERROR: $REPO doesn't exist."
+    exit 1
+fi
+
+# Shutdown st2
+cd $REPO
+tools/launchdev.sh stop
+if [[ $? != 0 ]]
+then
+    echo "ERROR: Unable to shut down st2."
+fi
+
+make distclean
+if [[ $? != 0 ]]
+then
+    echo "ERROR: make distclean failed in st2."
+fi
+
+# Kill mistral
 MISTRAL_PID=`ps auxww | grep mistral | grep -v grep | awk '{print $2}'`
 kill -s TERM $MISTRAL_PID
 
@@ -9,4 +33,14 @@ then
 else
     echo "ERROR: Could not kill mistral process with PID ${MISTRAL_PID}"
     exit 1
+fi
+
+# Wipe clean dbs
+if [ "$DROP_DBS" == "true" ] || [ "$DROP_DBS" == "True" ] || [ "$DROP_DBS" == "TRUE" ]
+then
+    mongo st2 --eval "db.dropDatabase();"
+    if [[ $? != 0 ]]
+    then
+        echo "ERROR: Unable to drop database 'st2' in mongo."
+    fi
 fi
