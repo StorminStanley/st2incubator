bca2e30d6c03a3885110277c17bd854040bb40fd
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2015-03-10 15:09:00 -0700

    Fix install_mistral and run_mistral scripts

--- packs/st2cd/actions/run_mistral.sh
+++ packs/st2cd/actions/run_mistral.sh
@@ -15,5 +15,21 @@ then
   exit 1
 fi
 
+## Check if mistral is already running
+netstat -tupln | grep 8989
+if [[ $? == 0 ]]
+then
+    echo "ERROR: Mistral is already running on the box."
+    exit 1  # Just exit for now.
+fi
+
+## Setup mistral logging
+LOG_CONFIG_FILE=$CONFIG_DIR/wf_trace_logging.conf
+cp $REPO/etc/wf_trace_logging.conf.sample $LOG_CONFIG_FILE
+LOG_FILE=/tmp/mistral-itests-$DATE.log
+sed -i s:/var/log/mistral.log:$LOG_FILE:g $LOG_CONFIG_FILE
+
 cd $REPO
-$REPO/.venv/bin/python $REPO/mistral/cmd/launch.py --config-file ${CONFIG_DIR}/mistral.conf 2>&1 > /tmp/mistral-itests-DATE.log
+CMD="$REPO/.venv/bin/python $REPO/mistral/cmd/launch.py --config-file ${CONFIG_DIR}/mistral.conf --log-config-append $LOG_CONFIG_FILE &"
+echo "Mistral command: $CMD"
+$CMD
