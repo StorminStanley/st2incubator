eec0901027b401f010ce83f4ed10a470e09aaf12
James Fryman <james@fryman.io>
2015-03-10 11:53:30 -0500

    savegame

--- packs/autoscale/sensors/autoscale_governor_sensor.py
+++ packs/autoscale/sensors/autoscale_governor_sensor.py
@@ -1,4 +1,5 @@
 import time
+import eventlet
 from st2reactor.sensor.base import PollingSensor
 
 __all_ = [
@@ -14,11 +15,12 @@ eventlet.monkey_patch(
 
 class AutoscaleGovernorSensor(PollingSensor):
     def __init__(self, sensor_service, config=None, poll_interval=None):
-        super(AutoscaleGovernorSensor, self).__init__(sensor_service=sensor_service,
-                                                      config=config,
-                                                      poll_interval=poll_interval)
+        self._config = config
+        self._sensor_service = sensor_service
+        self._poll_interval = poll_interval
         self._logger = self._sensor_service.get_logger(__name__)
         self._kvp_get = self.sensor_service.get_value
+
         self._trigger = {
             'expand': 'ScaleUpPulse',
             'deflate': 'ScaleDownPulse'
@@ -32,8 +34,8 @@ class AutoscaleGovernorSensor(PollingSensor):
         pass
 
     def poll(self):
-        alerting_apps = []
-        stable_apps   = []
+        alerting_asgs = []
+        stable_asgs   = []
 
         # Get all the ASG related keys in the Key Store
         kvps = self._sensor_service.list_values(local=False, prefix='asg.')
@@ -43,19 +45,28 @@ class AutoscaleGovernorSensor(PollingSensor):
             if 'active_incident' in kvp.name:
                 asg_data = kvp.name.split('.')
                 asg      = asg_data[1]
-                app_name = self._kvp_get("asg.%s.application_name".format(asg))
-                if kvp.value == True:
-                    alerting_apps.append(asg, app_name)
+                if bool(kvp.value):
+                    alerting_asgs.append(asg)
                 else:
-                    stable_apps.append(asg, app_name)
+                    stable_asgs.append(asg)
+
+
 
         # Attempt to determine if an ASG needs to scale up...
-        for app in alerting_apps:
-            self._process_app(asg, app, 'expand')
+        for asg in alerting_asgs:
+            payload = {
+                'asg': asg,
+            }
+            self._sensor_service.dispatch(trigger=trigger, payload=payload)
+            #self._process_asg(asg, 'expand')
 
         # ... or down
-        for app in stable_apps:
-            self._process_app(asg, app, 'deflate')
+        for asg in stable_asgs:
+            payload = {
+                'asg': asg,
+            }
+            self._sensor_service.dispatch(trigger=trigger, payload=payload)
+            #self._process_asg(asg, 'deflate')
 
     def cleanup(self):
         pass
@@ -69,7 +80,7 @@ class AutoscaleGovernorSensor(PollingSensor):
     def remove_trigger(self, trigger):
         pass
 
-    def _process_app(self, asg, app, action):
+    def _process_asg(self, asg, action):
         trigger_type         = self._trigger[action]
         bound                = self._bound[action]
         last_event_timestamp = self._kvp_get("asg.%s.last_%s_timestamp".format(asg, action))
