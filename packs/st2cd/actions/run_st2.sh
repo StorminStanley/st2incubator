bf9f4259044bdb3197050f34d99c009511a787c6
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2015-03-10 16:15:44 -0700

    Add run_st2 action

---
+++ packs/st2cd/actions/run_st2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PYTHON=`which python`
+REPO=$1
+DATE=`date +%s`
+
+if [[ ! -d $REPO ]]
+then
+    echo "ERROR: $REPO doesn't exist."
+    exit 1
+fi
+
+cd $REPO
+make requirements
+if [[ $? != 0 ]]
+then
+    echo "ERROR: Failed setting up st2 virtualenv."
+    exit 2
+fi
+
+if [[ ! -f $REPO/tools/launchdev.sh ]]
+then
+    echo "ERROR: launchdev.sh not found in $REPO/tools/"
+    exit 3
+fi
+
+echo "DEBUG: Launching st2..."
+$REPO/tools/launchdev.sh startclean
+
+if [[ $? != 0 ]]
+then
+    echo "ERROR: Failed to launch st2."
+    exit 4
+fi
