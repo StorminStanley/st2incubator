bf9b8df13b554e9f4720049ec8368a0eddc03846
manasdk <manasdk@gmail.com>
2015-03-23 14:52:17 -0700

    Update version in st2common/__init__ and st2client/__init__

--- packs/st2cd/actions/git_branch.sh
+++ packs/st2cd/actions/git_branch.sh
@@ -6,32 +6,40 @@ DATE=`date +%s`
 VERSION=$2
 BRANCH=$3
 BASE_BRANCH=$4
-FILE="st2common/st2common/__init__.py"
+ST2COMMON_INIT="st2common/st2common/__init__.py"
+ST2CLIENT_INIT="st2client/st2client/__init__.py"
 OUTPUT=/tmp/git-output-$DATE
 
 cd ${REPO}
 OUT=`$GIT checkout ${BASE_BRANCH} -q > $OUTPUT && $GIT pull origin ${BASE_BRANCH} -q > $OUTPUT && $GIT checkout -b ${BRANCH} -q`
 if [[ $? == 0 ]]
 then
-  VERSIONOUT=`sed -i -e "s/\(__version__ = \).*/\1'${VERSION}'/" ${FILE}`
+  VERSIONOUT=`sed -i -e "s/\(__version__ = \).*/\1'${VERSION}'/" ${ST2COMMON_INIT}`
   if [[ $? == 0 ]]
   then
-    OUT=`$GIT add $FILE > $OUTPUT && $GIT commit -aqm "Cutting branch for release - ${VERSION}" > $OUTPUT && $GIT push origin $BRANCH -q > $OUTPUT`
+    VERSIONOUT=`sed -i -e "s/\(__version__ = \).*/\1'${VERSION}'/" ${ST2CLIENT_INIT}`
+  else
+    echo ${OUTPUT}
+    exit 1
+  fi
+  if [[ $? == 0 ]]
+  then
+    OUT=`$GIT add $ST2COMMON_INIT $ST2CLIENT_INIT > $OUTPUT && $GIT commit -qm "Cutting branch for release - ${VERSION}" > $OUTPUT && $GIT push origin $BRANCH -q > $OUTPUT`
     if [[ $? == 0 ]]
     then
       echo ${BRANCH}
     else
       cat ${OUTPUT}
       rm ${OUTPUT}
-      exit 1
+      exit 2
     fi
   else
     cat ${OUTPUT}
     rm ${OUTPUT}
-    exit 2
+    exit 3
   fi
 else
   cat ${OUTPUT}
   rm ${OUTPUT}
-  exit 3
+  exit 4
 fi
