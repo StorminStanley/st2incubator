b9c0d8ecdf05b3e7159dc78fcc291314c92952fa
Patrick Hoolboom <patrick@stackstorm.com>
2015-06-03 12:59:17 -0700

    Adding mistral examples

---
+++ packs/st2cd/actions/install_examples.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DEBTEST=`lsb_release -a 2> /dev/null | grep Distributor | awk '{print $3}'`
+
+if [[ "$DEBTEST" == "Ubuntu" ]]; then
+  TYPE="debs"
+  PYTHONPACK="/usr/lib/python2.7/dist-packages"
+elif [[ -f "/etc/redhat-release" ]]; then
+  TYPE="rpms"
+  PYTHONPACK="/usr/lib/python2.7/site-packages"
+else
+  echo "Unknown Operating System"
+  exit 2
+fi
+
+INSTALL=`${PYTHONPACK}/st2common/bin/st2-setup-examples`
+
+EXITCODE=$?
+echo $INSTALL
+exit $EXITCODE
