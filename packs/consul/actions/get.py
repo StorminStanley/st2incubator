70278232fb2339a948ad72a95002fbc8b3c7099d
James Fryman <james@fryman.io>
2015-08-20 18:13:49 -0500

    destroying consul

--- packs/consul/actions/get.py
+++
@@ -1,6 +0,0 @@
-from lib import action
-
-class ConsulGetAction(action.ConsulBaseAction):
-    def run(self, key):
-        list, value = self.consul.kv.get(key)
-        return value
--- packs/consul/actions/list_services.py
+++
@@ -1,6 +0,0 @@
-from lib import action
-
-class ConsulListServicesAction(action.ConsulBaseAction):
-    def run(self):
-        index, services = self.consul.catalog.services()
-        return services
