e4c0268913dfd1270c85033629a766f2d2064a27
Winson Chan <m4d.coder@gmail.com>
2015-11-18 20:22:44 +0000

    Add step to capture psql deadlocks on st2ci.mistral failure

--- packs/st2ci/actions/backup_logs.sh
+++ packs/st2ci/actions/backup_logs.sh
@@ -19,5 +19,9 @@ if [[ ! -d "${BACKUP_DIR}" ]]; then
     mkdir -p ${BACKUP_DIR}
 fi
 
+# Backup st2 and mistral logs
 cp ${ST2_LOG_DIR}/*.log ${BACKUP_DIR}
 cp ${MISTRAL_LOG_DIR}/mistral*.log ${BACKUP_DIR} 
+
+# Capture any DB deadlocks
+sudo -u postgres psql -c "SELECT ka.query AS blocking, a.query AS blocked FROM pg_catalog.pg_locks bl JOIN pg_catalog.pg_stat_activity a ON a.pid = bl.pid JOIN pg_catalog.pg_locks kl ON kl.transactionid = bl.transactionid AND kl.pid != bl.pid JOIN pg_catalog.pg_stat_activity ka ON ka.pid = kl.pid WHERE NOT bl.GRANTED;" > ${BACKUP_DIR}/psql_deadlocks.log
