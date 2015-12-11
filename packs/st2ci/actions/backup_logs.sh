217bb6b081a61c7ead4cc07f5402d1573f421ae5
Winson Chan <m4d.coder@gmail.com>
2015-12-11 20:22:02 +0000

    Fix log backup in st2ci.mistral

--- packs/st2ci/actions/backup_logs.sh
+++ packs/st2ci/actions/backup_logs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-ST2_LOG_DIR=$1
+MISTRAL_REPO=$1
 MISTRAL_LOG_DIR=$2
-BACKUP_DIR=$3
+ST2_LOG_DIR=$3
+BACKUP_DIR=$4
 
 if [[ ! -d "${BACKUP_DIR}" ]]; then
     mkdir -p ${BACKUP_DIR}
@@ -24,10 +25,12 @@ if [[ -d "${MISTRAL_LOG_DIR}" ]]; then
     cp ${MISTRAL_LOG_DIR}/mistral*.log ${BACKUP_DIR} 
 fi
 
-# Backup the list of WF and task executions
-mistral execution-list > ${BACKUP_DIR}/mistral_execution_list.txt
-mistral task-list > ${BACKUP_DIR}/mistral_task_list.txt
-
 # Capture any DB deadlocks
 echo "Capturing DB logs..."
 sudo -u postgres psql -x -c "SELECT ka.query AS blocking, a.query AS blocked FROM pg_catalog.pg_locks bl JOIN pg_catalog.pg_stat_activity a ON a.pid = bl.pid JOIN pg_catalog.pg_locks kl ON kl.transactionid = bl.transactionid AND kl.pid != bl.pid JOIN pg_catalog.pg_stat_activity ka ON ka.pid = kl.pid WHERE NOT bl.GRANTED;" > ${BACKUP_DIR}/psql_deadlocks.log
+
+# Backup the list of WF and task executions
+. ${MISTRAL_REPO}/.venv/bin/activate
+mistral execution-list > ${BACKUP_DIR}/mistral_execution_list.txt
+mistral task-list > ${BACKUP_DIR}/mistral_task_list.txt
+deactivate
