44b8f7fee2ce40007cee2f6f7ea2438b21414523
Winson Chan <m4d.coder@gmail.com>
2015-12-14 20:39:59 +0000

    Fix log backup action in st2ci.mistral

--- packs/st2ci/actions/backup_logs.sh
+++ packs/st2ci/actions/backup_logs.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 set -e
 
-MISTRAL_REPO=$1
-MISTRAL_LOG_DIR=$2
-ST2_LOG_DIR=$3
-BACKUP_DIR=$4
-
-if [[ ! -d "${BACKUP_DIR}" ]]; then
-    mkdir -p ${BACKUP_DIR}
-fi
+ST2_EXEC_ID=$1
+MISTRAL_REPO=$2
+MISTRAL_LOG_DIR=$3
+ST2_LOG_DIR=$4
+BACKUP_DIR=$5
 
 # Cleanup anything from the backup directory older than a week
 find ${BACKUP_DIR}/* -type d -ctime +7 | xargs rm -rf
 
+# Create a new directory for this execution
+BACKUP_PATH=${BACKUP_DIR}/${ST2_EXEC_ID}
+if [[ ! -d "${BACKUP_PATH}" ]]; then
+    mkdir -p ${BACKUP_PATH}
+fi
+
 # Backup st2 logs
 if [[ -d "${ST2_LOG_DIR}" ]]; then
     echo "Backing up st2 logs..."
-    cp ${ST2_LOG_DIR}/*.log ${BACKUP_DIR}
+    cp ${ST2_LOG_DIR}/*.log ${BACKUP_PATH}
 fi
 
 # Backup mistral logs
 if [[ -d "${MISTRAL_LOG_DIR}" ]]; then
     echo "Backing up mistral logs..."
-    cp ${MISTRAL_LOG_DIR}/mistral*.log ${BACKUP_DIR} 
+    cp ${MISTRAL_LOG_DIR}/mistral*.log ${BACKUP_PATH} 
 fi
 
 # Capture any DB deadlocks
 echo "Capturing DB logs..."
-sudo -u postgres psql -x -c "SELECT ka.query AS blocking, a.query AS blocked FROM pg_catalog.pg_locks bl JOIN pg_catalog.pg_stat_activity a ON a.pid = bl.pid JOIN pg_catalog.pg_locks kl ON kl.transactionid = bl.transactionid AND kl.pid != bl.pid JOIN pg_catalog.pg_stat_activity ka ON ka.pid = kl.pid WHERE NOT bl.GRANTED;" > ${BACKUP_DIR}/psql_deadlocks.log
+sudo -u postgres psql -x -c "SELECT ka.query AS blocking, a.query AS blocked FROM pg_catalog.pg_locks bl JOIN pg_catalog.pg_stat_activity a ON a.pid = bl.pid JOIN pg_catalog.pg_locks kl ON kl.transactionid = bl.transactionid AND kl.pid != bl.pid JOIN pg_catalog.pg_stat_activity ka ON ka.pid = kl.pid WHERE NOT bl.GRANTED;" > ${BACKUP_PATH}/psql_deadlocks.log
+
+# Restart DB service to clear deadlocks so the steps below can run.
+sudo service postgresql restart
 
 # Backup the list of WF and task executions
 . ${MISTRAL_REPO}/.venv/bin/activate
-mistral execution-list > ${BACKUP_DIR}/mistral_execution_list.txt
-mistral task-list > ${BACKUP_DIR}/mistral_task_list.txt
+mistral execution-list > ${BACKUP_PATH}/mistral_execution_list.txt
+mistral task-list > ${BACKUP_PATH}/mistral_task_list.txt
 deactivate
