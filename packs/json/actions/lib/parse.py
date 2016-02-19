fd74b52ceb67a3126666901315ee2271338b890f
Edward Medvedev <edward.medvedev@gmail.com>
2016-02-19 20:04:13 +0600

    Removing `json`: outdated

--- packs/json/actions/lib/parse.py
+++
@@ -1,9 +0,0 @@
-from st2actions.runners.pythonrunner import Action
-import json
-
-class ParseJson(Action):
-    def run(self, string):
-        try:
-            return json.loads(string)
-        except (ValueError, KeyError, TypeError):
-            print "JSON format error"
