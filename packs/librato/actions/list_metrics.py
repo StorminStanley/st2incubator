80776c17f5e8841ae3a298727e1eda0cae47a853
Edward Medvedev <edward.medvedev@gmail.com>
2016-03-03 12:27:00 +0600

    Removing Librato: moved to contrib

--- packs/librato/actions/list_metrics.py
+++
@@ -1,5 +0,0 @@
-from lib import action
-
-class ListMetrics(action.LibratoBaseAction):
-    def run(self):
-        return self.librato.list_metrics()
--- packs/librato/actions/submit_gauge.py
+++
@@ -1,5 +0,0 @@
-from lib import action
-
-class LibratoSubmitGauge(action.LibratoBaseAction):
-    def run(self, name, value, source):
-        self.librato.submit(name, value, source=source)
