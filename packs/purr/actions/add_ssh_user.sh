b877a510ab2d780bf1636ee88fb9935d77d54da4
Winson Chan <m4d.coder@gmail.com>
2016-10-22 14:29:32 +0000

    Update add ssh user action

--- packs/purr/actions/add_ssh_user.sh
+++ packs/purr/actions/add_ssh_user.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 set -e
 
-SSHUSERS="sshusers"
 USERNAME=${1}
 PASSWORD=${2}
 
 if [ $(id -u ${USERNAME} &> /dev/null; echo $?) != 0 ]
 then
-    echo "########## Creating user: ${USERNAME} ##########"
-    useradd ${USERNAME}
-    echo -e "${PASSWORD}\n${PASSWORD}" | passwd ${USERNAME}
-    usermod –a –G ${SSHUSERS} ${USERNAME}
+    echo "Updating sshd_config to allow password authentication..."
+    sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+    service ssh restart
+
+    echo "Creating user ${USERNAME}..."
+    useradd -d /home/${USERNAME} -m ${USERNAME}
     chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}
 
     if [ $(grep ${USERNAME} /etc/sudoers.d/* &> /dev/null; echo $?) != 0 ]
     then
         echo "${USERNAME}    ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/st2
     fi
+
+    echo -e "${PASSWORD}\n${PASSWORD}" | passwd ${USERNAME}
 fi
